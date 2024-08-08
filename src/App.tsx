1c1256fcc958f4292ff5e4364bea572bdf52dd67
Rahul Mijar <rahulmijar0243@gmail.com>
2024-08-08 17:11:02 +0545

    feat: add about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppLayout from "./pages/AppLayout";
+import About from "./pages/About";
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AppLayout />}></Route>
+        <Route path="/" element={<AppLayout />}>
+          <Route path="/about" element={<About />} />
+        </Route>
       </Routes>
     </Router>
   );
--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -4,7 +4,7 @@ import Sidebar from "../layout/Sidebar";
 
 const AppLayout = () => {
   return (
-    <div className="flex flex-col-reverse md:flex-row md:gap-2 w-full rounded-lg md:p-2 h-screen">
+    <div className="flex flex-col-reverse md:flex-row md:gap-2 w-full rounded-lg md:p-2 md:h-screen">
       <div className="md:w-64 w-full">
         <Sidebar />
       </div>
